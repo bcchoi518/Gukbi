7d10d691f509bdb0bae0e4f8ffebe9a52a7c2157
bcchoi518 <bcchoi518@gamil.com>
2022-04-18 22:33:30 +0900

    6day_homework

--- 5_Java/220418/src/Quiz/Quiz00.java
+++ 5_Java/220418/src/Quiz/Quiz00.java
@@ -1,21 +1,21 @@
-package chap05;
+package Quiz;
 
 import java.util.Scanner;
 
-public class IfExam04 {
+public class Quiz00 {
 
 	public static void main(String[] args) {
 
-		final int DISTANCE = 400;	//상수
+		final int DISTANCE = 400; // 상수
 		int time = 0;
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("자동차의 최대 속도를 입력하세요 : ");
 		int maxSpeed = Integer.parseInt(sc.nextLine());
-		
+
 		time = DISTANCE / maxSpeed;
-		
+
 		if (time <= 2) {
 			System.out.println("High High Speed");
 		} else if (time <= 3) {
---
+++ 5_Java/220418/src/Quiz/Quiz03.java
@@ -0,0 +1,33 @@
+package Quiz;
+
+import java.util.Scanner;
+
+public class Quiz03 {
+
+	public static void main(String[] args) {
+
+//		성별이 여자이면서 토익 성적이 700점 이상인 경우 또는 성별이 남자이면서 토익 성적이
+//		800점 이상인 경우에만 "항공 승무원 지원 자격 가능자입니다"를 출력하는 프로그램을 작성하시오.
+
+		int score = 0;
+		String gender = "";
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("성별을 입력하세요 : ");
+		gender = sc.nextLine();
+		System.out.print("토익성적을 입력하세요 : ");
+		score = Integer.parseInt(sc.nextLine());
+
+		if (gender.equals("남자") && score >= 800) {
+			System.out.println("항공 승무원 지원 자격 가능자입니다.");
+		} else if (gender.equals("여자") && score >= 700) {
+			System.out.println("항공 승무원 지원 자격 가능자입니다.");
+		} else {
+			System.out.println("아쉽지만 지원 자격 미달입니다.");
+		}
+
+		sc.close();
+	}
+
+}
