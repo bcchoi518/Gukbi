283ae970e3d38b427eaf27881a1e894d61561236
bcchoi518 <bcchoi518@gmail.com>
2022-05-27 11:06:16 +0900

    java mini project ~ing

---
+++ 5_Java/Dasutflix/src/step9/ChoiceException.java
@@ -0,0 +1,26 @@
+package step9;
+
+public class ChoiceException extends Exception {
+	private int wrongIntChoice;
+	private String wrongStringChoice;
+
+	public ChoiceException(int choice) {
+		wrongIntChoice = choice;
+	}
+
+	public ChoiceException(String Input) {
+		wrongStringChoice = Input;
+	}
+
+	public void showErrorMessage() {
+		if (wrongStringChoice != null) {
+			//System.out.printf("[WARNNING] You have entered it incorrectly%n%n");
+			System.out.println();
+			System.out.println("    .....oOoOoOoOoOoOoOoOps!( You have entered it Incorrectly )");
+		} else {
+			System.out.println();
+			System.out.println("    .....oOoOoOoOoOoOoOoOps!( The choice corresponding to " + wrongIntChoice + " does not Exist");
+			//System.out.printf("[WARNNING] The choice corresponding to " + wrongIntChoice + " does not exist%n%n");
+		} // end if
+	}// end showErrorMessage
+}// end MenuChoiceException
---
+++ 5_Java/Dasutflix/src/step9/MovieView.java
@@ -0,0 +1,83 @@
+package step9;
+
+import java.util.Scanner;
+
+public class MovieView {
+	void movieShow(Movie selectMovie) {
+		System.out.println();
+		System.out.println("    ──────────────────────────── Title> " + selectMovie.title);
+		System.out.println("    ──────────────────────────── Score> " + selectMovie.score);
+		System.out.println("    ──────────────────────────── Synopsis> " + selectMovie.synopsis);
+		System.out.println("    ──────────────────────────── Genre> " + selectMovie.genre);
+	}
+
+	void showMovieStartMenu() {
+		System.out.println();
+		System.out.println("    ──────────────────────────── 1> Watch ");
+		System.out.println("    ──────────────────────────── 2> Add Favorites ");
+		System.out.println("    ──────────────────────────── 3> Exit ");
+		System.out.print("    ──────────────────────────── ◇  Choice> ");
+	}
+
+	void showMovieEndMenu() {
+		System.out.println();
+		System.out.println("    ──────────────────────────── 1> Rating ");
+		System.out.println("    ──────────────────────────── 2> Next Movie ");
+		System.out.println("    ──────────────────────────── 3> Exit ");
+		System.out.print("    ──────────────────────────── ◇  Choice> ");
+	}
+
+	// UserManager.getInstance().searchIsOnline().pf.saved
+	// MovieManager.getInstance().search(tmpMovie2)
+
+	void movieStart(int selectNumber) throws InterruptedException {
+		try {
+			movieShow(MovieManager.getInstance().movieStorage.get(selectNumber));
+			showMovieStartMenu();
+			int choice = Integer.parseInt(MenuViewer.sc.nextLine());
+			switch (choice) {
+			case 1:
+				Screen.getWatchScreen();
+				movieEnd(selectNumber);
+				break;
+			case 2:
+				UserManager.getInstance().searchIsOnline().pf.myList
+						.add(MovieManager.getInstance().movieStorage.get(selectNumber));// ?占쏙옙?占쏙옙
+				break;
+			case 3:
+				return;
+			}
+		} catch (InterruptedException ex) {
+		    Thread.currentThread().interrupt();
+		}
+	}
+
+	void movieEnd(int selectNumber) throws InterruptedException { //변경점 시작
+		try {
+			showMovieEndMenu();
+			int choice = Integer.parseInt(MenuViewer.sc.nextLine());
+			switch (choice) {
+			case 1:
+				System.out.print("    ──────────────────────────── Enter Rating Score Between 0 and 5> ");
+				double userScore = Double.parseDouble(MenuViewer.sc.nextLine());
+				MovieManager.getInstance().movieStorage.get(selectNumber).setScore(userScore);
+				System.out.println("    ──────────────────────────── Rating Complete ! ");
+			case 2:
+				if(selectNumber==15) {
+					selectNumber=1;
+					movieStart(selectNumber);
+				} else if (selectNumber==20) {
+					selectNumber=16;
+					movieStart(selectNumber);
+				} else {
+					movieStart(selectNumber+1);
+				}
+				break;
+			case 3:
+				return;
+			}
+		} catch (InterruptedException ex) {
+		    Thread.currentThread().interrupt();
+		}
+	} //변경점 끝
+}
