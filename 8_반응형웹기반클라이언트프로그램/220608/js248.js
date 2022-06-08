eb8877b0caa9c421f17d2dba92ae94dee88f5312
gukbi_com <gukbi_com>
2022-06-08 17:29:20 +0900

    39day

---
+++ 8_반응형웹기반클라이언트프로그램/220608/js248.js
@@ -0,0 +1,9 @@
+var subject1 = 'Welcome to Javascript!<br>';
+var subject2 = 'Enjoy Javascript Programming!';
+
+function welcome(target) {
+  document.write(target);
+}
+
+welcome(subject1);
+welcome(subject2);
