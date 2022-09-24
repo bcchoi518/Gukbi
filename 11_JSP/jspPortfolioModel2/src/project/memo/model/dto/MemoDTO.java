fe2524cb83814587abdd60e928c96e7417fdaf0f
bcchoi518 <bcchoi518@gamil.com>
2022-09-24 16:22:22 +0900

    update jspPortfolioModel2 memo

--- 11_JSP/jspPortfolioModel2/src/project/memo/model/dto/MemoDTO.java
+++ 11_JSP/jspPortfolioModel2/src/project/memo/model/dto/MemoDTO.java
@@ -1,5 +1,63 @@
 package project.memo.model.dto;
 
-public class MemoDTO {
+import java.sql.Date;
 
+public class MemoDTO {
+    private int no;
+    private String writer;
+    private String content;
+    private Date regiDate;
+    private String searchGubun;
+    private String searchData;
+    private int startRecord;
+    private int lastRecord;
+    
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getRegiDate() {
+		return regiDate;
+	}
+	public void setRegiDate(Date regiDate) {
+		this.regiDate = regiDate;
+	}
+	public String getSearchGubun() {
+		return searchGubun;
+	}
+	public void setSearchGubun(String searchGubun) {
+		this.searchGubun = searchGubun;
+	}
+	public String getSearchData() {
+		return searchData;
+	}
+	public void setSearchData(String searchData) {
+		this.searchData = searchData;
+	}
+	public int getStartRecord() {
+		return startRecord;
+	}
+	public void setStartRecord(int startRecord) {
+		this.startRecord = startRecord;
+	}
+	public int getLastRecord() {
+		return lastRecord;
+	}
+	public void setLastRecord(int lastRecord) {
+		this.lastRecord = lastRecord;
+	}
 }//MemoDTO
