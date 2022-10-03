9f591f5e46d407e11cd3024b5296d846e630d65f
Gukbi_com <Gukbi_com>
2022-10-03 17:32:48 +0900

    114day

---
+++ 12_spring/springStudy/src/main/java/com/cbc/springStudy/_common/Constants.java
@@ -0,0 +1,6 @@
+package com.cbc.springStudy._common;
+
+public class Constants {
+	public static final String ATTACH_PATH = "C:/cbc/attach";
+	public static final int MAX_UPLOAD = 1024 * 1024 * 10;
+}//Constants
---
+++ 12_spring/sql/221003_local_springStudy.sql
@@ -0,0 +1,24 @@
+CREATE TABLE memo (
+    no NUMBER NOT NULL,
+    writer VARCHAR2(50) NOT NULL,
+    content CLOB NOT NULL,
+    regiDate DATE NOT NULL,
+    PRIMARY KEY (no)
+);
+
+CREATE SEQUENCE seq_memo START WITH 1 INCREMENT BY 1 NOMAXVALUE NOCACHE;
+
+CREATE TABLE guestBook (
+    no NUMBER NOT NULL,
+    name VARCHAR2(50) NOT NULL,
+    email VARCHAR2(50) NOT NULL,
+    passwd VARCHAR2(50) NOT NULL,
+    content CLOB NOT NULL,
+    regiDate DATE NOT NULL,
+    memberNo NUMBER NOT NULL,
+    PRIMARY KEY (no)
+);
+
+CREATE SEQUENCE seq_guestBook START WITH 1 INCREMENT BY 1 NOMAXVALUE NOCACHE;
+
+select * from guestbook;
